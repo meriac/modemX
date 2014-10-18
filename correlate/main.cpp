2e335cad60874d03a65bc44e88dd434cb8e5c495
Milosch Meriac <milosch@meriac.com>
2014-10-18 22:59:54 +0100

    pre-calculate symbols

--- correlate/main.cpp
+++ correlate/main.cpp
@@ -16,10 +16,10 @@
 #define SAMPLES_PER_CYCLE (FREQ_SAMPLING_RATE/CARRIER_FREQ)
 #define SAMPLES_PER_SYMBOL (AES_BLOCKS*AES_BLOCK_SIZE*8*CYCLES_PER_SYMBOL*SAMPLES_PER_CYCLE)
 
-#define CORRELATOR_COUNT 2
+#define SYMBOL_COUNT 2
 
 static double g_pos, g_step;
-static int16_t g_buffer[SAMPLES_PER_SYMBOL];
+static int16_t g_symbol[SYMBOL_COUNT][SAMPLES_PER_SYMBOL];
 
 void symbol(uint8_t data, int16_t* dst)
 {
@@ -60,6 +60,10 @@ int main(int argc, char * argv[])
 	int i,j,t;
 	int16_t sample;
 
+	/* init correlators */
+	for(t=0; t<2; t++)
+		symbol(t, g_symbol[t]);
+
 	g_pos = 0;
 	g_step = (2*M_PI*CARRIER_FREQ)/FREQ_SAMPLING_RATE;
 
@@ -67,10 +71,9 @@ int main(int argc, char * argv[])
 	{
 		for(j=0; j<2; j++)
 		{
-			symbol(j, g_buffer);
 			for(i=0; i<SAMPLES_PER_SYMBOL; i++)
 			{
-				sample = g_buffer[i];
+				sample = g_symbol[j][i];
 				putchar((sample >> 0) & 0xFF);
 				putchar((sample >> 8) & 0xFF);
 			}
